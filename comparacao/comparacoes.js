7827439548b508d8c16f1b38c1b50b04d0b5ac3b
thalialima <thaliairene20@gmail.com>
2021-11-11 17:47:44 -0300

    Operadores de comparação

---
+++ comparacao/comparacoes.js
@@ -0,0 +1,18 @@
+// == comparação implícita (compara só o valor)
+
+const numero = 5;
+const texto = '5';
+// faz a cnversão
+console.log(numero == texto);
+
+// === comparação explícita (compara o valor o tipo de dado)
+
+console.log(numero === texto);
+
+// typeof serve para descobrir qual o tipo de dado da variável
+
+console.log(typeof numero);
+console.log(typeof numero);
+
+/* as boas práticas recomendam sempre o uso de '==='*/
+/* e conversões devem ser feitas sempre de forma explícita com as funções de conversão*/
