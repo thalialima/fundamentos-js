c829e429fc5256c02c673021829af050d2bf86bc
thalialima <thaliairene20@gmail.com>
2021-11-11 22:34:18 -0300

    Template string

---
+++ concatenacao/template-string.js
@@ -0,0 +1,10 @@
+const nome = 'Thalia';
+const cidadeNatal = 'Várzea Alegre';
+const apresentacao = 'Meu nome é ' + nome + ', nasci em ' + cidadeNatal;
+
+console.log(apresentacao);
+
+//template string
+const apresentacaoComTemplateString = `Meu nome é ${nome}, nasci em ${cidadeNatal}`;
+
+console.log(apresentacaoComTemplateString);
